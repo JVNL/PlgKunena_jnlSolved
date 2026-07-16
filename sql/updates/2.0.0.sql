9b0f508edee695314e4e615c27583458933c7cdb
Astrid van Eerd <Astrid@yndi.nl>
2026-07-16 10:51:38 +0200

    v2.0.3: definitief verwijderen van sql/updates/2.0.0.sql

--- sql/updates/2.0.0.sql
+++
@@ -1,4 +0,0 @@
--- Upgrade from 1.x: add an index on topicid, used by every lookup this
--- plugin does. Safe to run once; if you ever need to run it manually
--- twice, drop the index first (`ALTER TABLE #__kunena_jnlsolved DROP INDEX idx_jnlsolved_topicid;`).
-ALTER TABLE `#__kunena_jnlsolved` ADD INDEX `idx_jnlsolved_topicid` (`topicid`);
